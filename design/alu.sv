2d0cff4faeb4ddd88fb0a08f033c6cf2a4969b74
anshulrao0403 <51308061+anshulrao0403@users.noreply.github.com>
2023-04-27 00:13:11 +0200

    Update alu.sv
    I have added the operations into the ALU

--- design/alu.sv
+++ design/alu.sv
@@ -3,11 +3,11 @@ import common::*;
 module moduleName #(
         int DATA_WIDTH = 32
 ) (
-        input [3:0]             operation,
-        input [DATA_WIDTH-1:0]  left_operand,
-        input [DATA_WIDTH-1:0]  right_operand,
+        input [4:0] operation,
+        input [DATA_WIDTH-1:0]  A,
+        input [DATA_WIDTH-1:0]  B,
 
-        output logic                 zero,
+        output logic zero,
         output logic [DATA_WIDTH-1:0] result // ???
 );
 
@@ -15,9 +15,26 @@ module moduleName #(
 logic [DATA_WIDTH-1:0] alu_result;
 
 always_comb begin : ALU
+
+// shift amount for SLL, SLLI, SRL and SRLI defined by shamt
+shamt = B[4:0];
+
         case (operation)
-                AND:    alu_result = left_operand & right_operand;
-                OR:     alu_result = left_operand | right_operand;
+                SLL  :    alu_result = A << shamt;
+                SLLI :    alu_result = A << shamt;
+                SRL  :    alu_result = A >> shamt;
+                SRLI :    alu_result = A >> shamt;
+                ADD  :    alu_result = A + B;
+                ADDI :    alu_result = A + B;
+                SUB  :    alu_result = A - B;
+                LUI  :    alu_result = {B, 12'h0};
+                XOR  :    alu_result = A ^ B;
+                XORI :    alu_result = A ^ {B[31:0], {20{1'b0}}};
+                OR   :    alu_result = A | B;
+                ORI  :    alu_result = A | {B[31:0], {20{1'b0}}};
+                AND  :    alu_result = A & B;
+                ANDI :    alu_result = A & {B[31:0], {20{1'b0}}};
+                
                 default: 
                         $error("No such operation!!, %d\n", operation);
         endcase
