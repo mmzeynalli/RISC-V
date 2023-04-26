ef51ecd1089cab55c06b3895bc2118517556b75a
anshulrao0403 <51308061+anshulrao0403@users.noreply.github.com>
2023-04-27 00:39:10 +0200

    Update alu.sv

--- design/alu.sv
+++ design/alu.sv
@@ -13,6 +13,9 @@ module moduleName #(
 
 
 logic [DATA_WIDTH-1:0] alu_result;
+wire        sltResult;
+wire        sltuResult;
+wire        sraResult;
 
 always_comb begin : ALU
 
@@ -34,7 +37,27 @@ shamt = B[4:0];
                 ORI  :    alu_result = A | {B[31:0], {20{1'b0}}};
                 AND  :    alu_result = A & B;
                 ANDI :    alu_result = A & {B[31:0], {20{1'b0}}};
-                
+                SLT  :  begin
+                        if (A < B)
+                                sltResult = 1;
+                        else
+                                sltResult = 0;
+                        alu_result = { {32{sltResult}}, 1'b0 };
+                        end
+                SLTU :  begin
+                        if (unsigned(A) < unsigned(B))
+                                sltuResult = 1;
+                        else
+                                sltuResult = 0;
+                        alu_result = { {32{sltuResult}}, 1'b0 };
+                        end
+                SRA :   begin
+                        if (B[4:0] > 31)
+                                sraResult = { {32{A[31]}}, A[31:1] };
+                        else
+                                sraResult = { {B[4:0], A[31]} , A[31:B[4:0]+1] };
+                        alu_result = sraResult;
+                        end
                 default: 
                         $error("No such operation!!, %d\n", operation);
         endcase
