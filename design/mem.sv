563e36be8f93be90bae87c9a9ac3b012ef7feb44
anshulrao0403 <51308061+anshulrao0403@users.noreply.github.com>
2023-05-08 15:14:40 +0200

    Updated mem.sv and wb.sv
    Fixed the code on the mem and wb stages with all combinational statements and blocks

--- design/mem.sv
+++ design/mem.sv
@@ -1,61 +1,58 @@
 import common::*;
 
 module memory (
-        input logic clk,
-        input logic rst,
-        input logic control_mem_read,
-        input logic control_mem_write,
-        input logic control_reg_write,
-        input logic control_mem_reg,
-        input  reg [31:0] register_file_data2,
-        input  reg [31:0] alu_result,
-        input  reg [4:0] register_file_rd,
-        input  reg [INSTRUCTION_WIDTH-1 : 0] data_read;
-        output logic data_write_en,
-        output reg [PROGRAM_ADDRESS_WIDTH-1:0] data_address,
-        output reg [INSTRUCTION_WIDTH-1 : 0] data_write,
-        output reg [31:0] alu_result_out,
-        output reg [4:0] register_file_rd_out
+
+        input  logic control_mem_read,
+        input  logic control_mem_write,
+        input  logic control_reg_write,
+        input  logic control_mem_to_reg,
+        input  logic [31:0] register_file_data2,
+        input  logic [31:0] alu_result,
+        input  logic [4:0] register_file_rd,
+        input  logic [INSTRUCTION_WIDTH-1 : 0] mem_data_read;
+        output logic mem_data_write_en,
+        output logic [PROGRAM_ADDRESS_WIDTH-1:0] mem_data_address,
+        output logic [INSTRUCTION_WIDTH-1 : 0] mem_data_write,
+        output logic [31:0] alu_result_o,
+        output logic [31:0] mem_data_o,
+        output logic [4:0] register_file_rd_o,
+        output logic control_reg_write_o,
+        output logic control_mem_to_reg_o
         
 );
 
 
-reg [PROGRAM_ADDRESS_WIDTH-1:0] address_reg;
-reg [INSTRUCTION_WIDTH-1 : 0] write_reg;
-reg [PROGRAM_ADDRESS_WIDTH-1:0] alu_out_reg;
-reg [4:0] rd_out_reg;
-logic enable;
-
 
 always @(*) begin
-        if (rst) begin
-                address_reg = 32'd0;
-                write_reg   = 32'd0;
-                alu_out_reg = 32'd0;
-                rd_out_reg  = 32'd0; // reset
+         if ((control_mem_read == 1)) begin // LOAD operation
+                mem_data_write_en = 1b'0;
+                alu_result_o = alu_result;
+                mem_data_o = mem_data_read;
+                mem_data_address = alu_result;
+                register_file_rd_o = register_file_rd;
+                control_reg_write_o = control_reg_write;
+                control_mem_to_reg_o = control_mem_to_reg;
+
+
         end
-        else if ((control_mem_read) && (control_reg_write) && (control_mem_to_reg)) begin // LOAD operation
-                enable = 1'b0;
-                rd_out_reg  = register_file_rd;
-        end
-        else if ((control_write)) begin // STORE operation
-                address_reg = alu_result;
-                write_reg = register_file_data2;
-                enable = 1'b1;
-                rd_out_reg  = register_file_rd;
+        else if ((control_write == 1)) begin // STORE operation
+                mem_data_write_en = 1b'1;
+                alu_result_o = alu_result;
+                mem_data_o = mem_data_read;
+                mem_data_address = alu_result;
+                register_file_rd_o= register_file_rd;
+                control_reg_write_o = control_reg_write;
+                control_mem_to_reg_o = control_mem_to_reg;
         end
         else begin // all other operations
-               alu_out_reg = alu_result;
-               rd_out_reg  = register_file_rd; 
+               alu_result_o = alu_result;
+               mem_data_o = mem_data_read;
+               register_file_rd_o = register_file_rd;
+               control_reg_write_o = control_reg_write;
+               control_mem_to_reg_o = control_mem_to_reg;
         end
 end
 
-//OUTPUTS to the module 
 
-assign data_write_en = enable;
-assign data_address = address_reg;
-assign data_write = write_reg;
-assign alu_result_out = alu_out_reg;
-assign register_file_rd_out = rd_out_reg;
 
 endmodule
