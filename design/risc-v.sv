7a1c6ace2da191d4d507c5180a414663e6b164fa
mmzeynalli <miradil.zeynalli@gmail.com>
2023-05-10 01:12:54 +0200

    Initialized top design

--- design/risc-v.sv
+++ design/risc-v.sv
@@ -6,22 +6,48 @@ module risc_v #(
         int REGISTER_FILE_ADDRESS_WIDTH = 5
 ) (
         input clk,
-        input rst,
-        
-        
-        input [INSTRUCTION_WIDTH-1:0] program_data,
-        output [PROGRAM_ADDRESS_WIDTH-1:0] pc,
-
-        output [DATA_ADDRESS_WIDTH-1:0] data_address,
-        input [CPU_DATA_WIDTH-1:0] data_read,
-        output data_write_en,
-        output [CPU_DATA_WIDTH-1:0] data_write
-
+        input rst
 );
 
 
+logic [PROGRAM_ADDRESS_WIDTH - 1:0] pc, pc_next;
+logic [31:0] instruction, instruction_next;
 
+instruction_fetch if_stage(
+        .clk(clk),
+        .rst(rst),
+        .pc_out(pc_next),
+        .instruction(instruction)
+);
 
+instruction_decode id_stage(
+        .instruction(instruction)
+
+);
+
+execute ex_stage(
+
+);
+
+memory mem_stage(
+        
+);
+
+write_back wb_stage(
+
+);
+
+always @(posedge clk) begin
+        if (rst == RESET)
+        begin
+                pc = 6'b0;
+                instruction = 32'b0;
+        end
+        else
+        begin
+                pc = pc_next;
+        end
+end
 
         
 endmodule
