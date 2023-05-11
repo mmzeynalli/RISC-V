827a1817e8bc60cc40f681a94cfd6f54fc33f31c
anshulrao0403 <51308061+anshulrao0403@users.noreply.github.com>
2023-05-11 09:18:48 +0200

    Update mem.sv

--- design/mem.sv
+++ design/mem.sv
@@ -1,60 +1,39 @@
 import common::*;
 
 module memory (
-        input  logic clk,
-        input logic rst,
-        input logic control_mem_read,
-        input logic control_mem_write,
-        input logic control_reg_write,
-        input logic control_mem_reg,
-        input  reg [31:0] register_file_data2,
-        input  reg [31:0] alu_result,
-        input  reg [4:0] register_file_rd,
-        input  reg [INSTRUCTION_WIDTH-1 : 0] data_read;
-        output logic data_write_en,
-        output reg [PROGRAM_ADDRESS_WIDTH-1:0] data_address,
-        output reg [INSTRUCTION_WIDTH-1 : 0] data_write,
-        output reg [31:0] alu_result_out,
-        output reg [4:0] register_file_rd_out
+        input [OPERAND_WIDTH-1:0] alu_result,
+        input [OPERAND_WIDTH-1:0] rs2_data,
+        input [31:0] mem_data_read,
 
+        // Controls
+        input ctrl_mem_read,
+        input ctrl_mem_write,
+        input ctrl_mem_to_reg,
+
+        output logic mem_data_write_en,
+        output logic [PROGRAM_ADDRESS_WIDTH-1:0] mem_data_address,
+        output logic [INSTRUCTION_WIDTH-1:0] mem_data_write,
+        output logic [31:0] o_mem_data
 );
 
 
-reg [PROGRAM_ADDRESS_WIDTH-1:0] address_reg;
-reg [INSTRUCTION_WIDTH-1 : 0] write_reg;
-reg [PROGRAM_ADDRESS_WIDTH-1:0] alu_out_reg;
-reg [4:0] rd_out_reg;
-logic enable;
-
 
 always @(*) begin
-        if (rst) begin
-                address_reg = 32'd0;
-                write_reg   = 32'd0;
-                alu_out_reg = 32'd0;
-                rd_out_reg  = 32'd0; // reset
+         if (ctrl_mem_read == 1) begin // LOAD operation
+                mem_data_write_en = 1'b0;
+                o_mem_data = mem_data_read;
+                mem_data_address = alu_result;
         end
-        else if ((control_mem_read) && (control_reg_write) && (control_mem_to_reg)) begin // LOAD operation
-                enable = 1'b0;
-                rd_out_reg  = register_file_rd;
-        end
-        else if ((control_write)) begin // STORE operation
-                address_reg = alu_result;
-                write_reg = register_file_data2;
-                enable = 1'b1;
-                rd_out_reg  = register_file_rd;
+        else if (ctrl_mem_write == 1) begin // STORE operation
+                mem_data_write_en = 1'b1;
+                o_mem_data = mem_data_read;
+                mem_data_address = alu_result;
         end
         else begin // all other operations
-               alu_out_reg = alu_result;
-               rd_out_reg  = register_file_rd; 
+               o_mem_data = mem_data_read;
         end
 end
 
-//OUTPUTS to the module 
 
-assign data_write_en = enable;
-assign data_address = address_reg;
-assign data_write = write_reg;
-assign alu_result_out = alu_out_reg;
-assign register_file_rd_out = rd_out_reg;
+
 endmodule
