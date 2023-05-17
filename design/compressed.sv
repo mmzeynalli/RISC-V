2dd7ff5cd6faace5ab8f14e203ee2abefbaa8b7e
anshulrao0403 <51308061+anshulrao0403@users.noreply.github.com>
2023-05-17 11:19:02 +0200

    updated compressed.sv
    Added compresssed instructions, updated pc count and mapped it to ID file

--- design/compressed.sv
+++ design/compressed.sv
@@ -1,78 +1,85 @@
 import common::*;
 
-module compressed ()
+module compressed (
+    input [INSTRUCTION_WIDTH-1:0] mem_instruction,
+
+    output compressed,
+    output [INSTRUCTION_WIDTH-1:0] instruction
+
+
+)
 
 always_comb begin : compressed_check
-        if (instruction[1:0] == '11' )
+        if (mem_instruction[1:0] == 'b11 )
             compressed = 1'b0;
         else 
             compressed = 1'b1;
 end 
 
 always_comb begin : compressed_expand
-        if (compressed == 1'b1)
-             if ( instruction[1:0] == 'b00) begin
-                    if      ( instruction[15:13] == 'b010) //C.LW
-                            o_instruction = 5'b0, instruction[5], instruction[12:10], instruction[6], 2'b0, 2'b0, instruction[9:7], instruction[15:13], 2'b0, instruction[4:2], 7'b0000011;
-                    else if ( instruction[15:13] == 'b110) //C.SW
-                            o_instruction = 5'b0, instruction[5], instruction[12], 2'b0, instruction[4:2], 2'b0, instruction[9:7], instruction[15:13], instruction[11:10],instruction[6], 2'b0, 7'b0100011;
-             end
+        case(compressed)
+        1'b1:begin
+              
+                case(mem_instruction[1:0])
+                2'b00:begin
+                        if ( mem_instruction[15:13] == 'b010) //C.LW
+                                instruction = instruction{5'b0, mem_instruction[5], mem_instruction[12:10], mem_instruction[6], 2'b0, 2'b0, mem_instruction[9:7], mem_instruction[15:13], 2'b0, mem_instruction[4:2], 7'b0000011};
+                        else if ( mem_instruction[15:13] == 'b110) //C.SW
+                                instruction = instruction{5'b0, mem_instruction[5], mem_instruction[12], 2'b0, mem_instruction[4:2], 2'b0, mem_instruction[9:7], mem_instruction[15:13], mem_instruction[11:10],mem_instruction[6], 2'b0, 7'b0100011};
+                end
+                2'b10:begin
+                        if( mem_instruction [15:13] == 'b000)   //C.SLLI
+                                instruction = instruction{7'b0, mem_instruction[12], mem_instruction[6:2], mem_instruction[11:7], 3'b001, mem_instruction[11:7], 7'b0010011;}
+                        else if ( mem_instruction[15:13] == 'b100) //C.ADD
+                                instruction = instruction{7'b0, mem_instruction[6:2], mem_instruction[11:7], 3'b000, mem_instruction[11:7], 7'b0110011};
+                end
+                2'b01:begin
+                        if      ( mem_instruction[15:13] == 'b000)// C.ADDI
+                                instruction = instruction{7'b0, mem_instruction[12],mem_instruction[6:2], mem_instruction[11:7], 3'b000, mem_instruction[11:7],7'b0010011};
+                        else if ( mem_instruction[15:13] == 'b010) //C.LI
+                                instruction = instruction{7'b0, mem_instruction[12],mem_instruction[6:2], mem_instruction[11:7], 8'b0, 7'b0010011};
+                        else if ( mem_instruction[15:13] == 'b011) //C.LUI
+                                instruction = instruction {14'b0, mem_instruction[12],mem_instruction[6:2], mem_instruction[11:7], 7'b0110111};
+                        else if ( mem_instruction[15:13] == 'b100 ) begin
 
-             else if ( instruction[1:0] == 'b10) begin
-                    if( instruction [15:13] == 'b000)   //C.SLLI
-                            o_instruction = 7'b0, instruction[12], instruction[6:2], instruction[11:7], 3'b001, instruction[11:7], 7'b0010011;
-                    else if ( instruction[15:13] == 'b100) //C.ADD
-                            o_instruction = 7'b0, instruction[6:2], instruction[11:7], 3'b000, instruction[11:7], 7'b0110011;
-             end
-            
-             else if  ( instruction[1:0] == 'b01) begin
-                       if ( instruction[15:13] == 'b000)// C.ADDI
-                                    o_instruction = 7'b0, instruction[12],instruction[6:2], instruction[11:7], 3'b000, instruction[11:7],7'b0010011;
-                       else if ( instruction[15:13] == 'b010) //C.LI
-                                    o_instruction = 7'b0, instruction[12],instruction[6:2], instruction[11:7], 8'b0, 7'b0010011;
-                       else if ( instruction[15:13] == 'b011) //C.LUI
-                                    o_instruction = 14'b0, instruction[12],instruction[6:2], instruction[11:7], 7'b0110111;
-                       else if ( instruction[15:13] == 'b100 ) begin
-                                         if ( instruction[11:10] == 'b00 )// C.SRLI
-                                                 o_instruction = 7'b0, instruction[12], instruction[6:2], 2'b0, instruction[9:7], 3'b101, 2'b0, instruction[9:7], 7'b0010011;
-                                         else if ( instruction[11:10] == 'b00 )// C.SRAI
-                                                 o_instruction = 7'b0100000, instruction[12], instruction[6:2], 2'b0, instruction[9:7], 3'b101, 2'b0, instruction[9:7], 7'b0010011;
-                                         else if ( instruction[11:10] == 'b00 )// C.ANDI
-                                                 o_instruction = 7'b0, instruction[12],instruction[6:2], 2'b0, instruction[9:7], 3'b111, 2'b0, instruction[9:7],7'b0010011;
-                                         else begin 
-                                                       if (instruction[6:5] == 'b00)// C.SUB
-                                                                o_instruction = 7'b0100000, instruction[6:2], 2'b0, instruction[9:7], 3'b000,2'b0, instruction[9:7], 7'b0110011;
-                                                       else if (instruction[6:5] == 'b01)//C.XOR
-                                                                o_instruction = 7'b0, 2'b0, instruction[4:2], 2'b0, instruction[9:7], 3'b111,2'b0, instruction[9:7], 7'b0110011;
-                                                       else if  (instruction[6:5] == 'b10)//C.OR
-                                                                o_instruction = 7'b0, 2'b0, instruction[4:2], 2'b0, instruction[9:7], 3'b110, 2'b0, instruction[9:7], 7'b0110011;
-                                                       else // C.AND
-                                                                o_instruction = 7'b0, 2'b0, instruction[4:2], 2'b0, instruction[9:7], 3'b111, 2'b0, instruction[9:7], 7'b0110011;
-                                         end
+                                case(mem_instruction[11:10])
+                                2'b00: //C.SRLI 
+                                        instruction = instruction{7'b0, mem_instruction[12], mem_instruction[6:2], 2'b0, mem_instruction[9:7], 3'b101, 2'b0, mem_instruction[9:7], 7'b0010011};
+                                2'b01: //C.SRAI
+                                        instruction = instruction{7'b0100000, mem_instruction[12], mem_instruction[6:2], 2'b0, mem_instruction[9:7], 3'b101, 2'b0, mem_instruction[9:7], 7'b0010011};
+                                2'b10: //C.ANDI
+                                        instruction = instruction{7'b0, mem_instruction[12],mem_instruction[6:2], 2'b0, mem_instruction[9:7], 3'b111, 2'b0, mem_instruction[9:7],7'b0010011};      
+                                2'b11:begin
 
-                        
+                                        case(mem_instruction[6:5])
+                                        2'b00: //C.SUB
+                                                instruction = instruction{7'b0100000, mem_instruction[6:2], 2'b0, mem_instruction[9:7], 3'b000,2'b0, mem_instruction[9:7], 7'b0110011};
+                                        2'b01: //C.XOR
+                                                instruction = instruction{7'b0, 2'b0, mem_instruction[4:2], 2'b0, mem_instruction[9:7], 3'b111,2'b0, mem_instruction[9:7], 7'b0110011};
+                                        2'b10: //C.OR
+                                                instruction = instruction{7'b0, 2'b0, mem_instruction[4:2], 2'b0, mem_instruction[9:7], 3'b110, 2'b0, mem_instruction[9:7], 7'b0110011};
+                                        2'b11: //C>AND
+                                                instruction = instruction{7'b0, 2'b0, mem_instruction[4:2], 2'b0, mem_instruction[9:7], 3'b111, 2'b0, mem_instruction[9:7], 7'b0110011}; 
+                                        default:
+                                                $error("Unknown Instruction");
+                                        endcase
+                                end
+                                default:
+                                        $error("unknown instruction");
+                                endcase
+
                         end
-            end
+                end
+                default: 
+                        $error("unknown instruction");
+                endcase
+        end
+        1'b0:    
+                   instruction = mem_instruction;
+        default: 
+                   $error("unknown instruction");
+        endcase                  
 end
             
                     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 endmodule
--- design/if.sv
+++ design/if.sv
@@ -8,6 +8,7 @@ module instruction_fetch import common::*;
         input [IMM_WIDTH-1:0] imm,
         input ctrl_branch_taken,
         input ctrl_jump_taken,
+        input compressed,
 
         // output  logic [PROGRAM_ADDRESS_WIDTH-1:0] o_pc, 
         // output  logic [PROGRAM_ADDRESS_WIDTH-1:0] o_pc_4, 
@@ -35,6 +36,11 @@ always_comb begin : next_pc_selection
         pc_next = pc + 4;
         instruction <= instr;
 
+        if (compressed) // compressed instructions
+        begin
+                pc_next = pc + 2
+                instruction <= instr;
+        end
         if (ctrl_branch_taken) // conditional branch
         begin
                 pc_next = pc - 4 + 32'(signed'(imm));
@@ -47,6 +53,7 @@ always_comb begin : next_pc_selection
         end
         else if (stall) // Stall condition
                 pc_next = pc;
+                instruction <== 'h13;
 
 end
 
@@ -57,6 +64,8 @@ instruction_memory instruction_memory(
         .address(pc),
         .read_data(instr)
 );
+
+
 
 
 endmodule
