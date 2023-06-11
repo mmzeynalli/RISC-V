25ee995923b92afb2fa1054b98f3e393889c6bf3
mmzeynalli <miradil.zeynalli@gmail.com>
2023-06-11 13:10:44 +0400

    Fixing division

--- design/int_div.sv
+++ design/int_div.sv
@@ -35,32 +35,6 @@ logic [4:0] i, max_iter;      // iteration counter
 logic [OPERAND_WIDTH-1:0] dividend, divisor;
 logic a_sign, b_sign;
 
-always_comb begin : handle_sign
-
-        if (start)
-        begin
-                a_sign = 0;
-                b_sign = 0;
-                dividend = a;
-                divisor = b;
-
-                if (is_signed)
-                begin
-                        if (a[OPERAND_WIDTH-1])
-                        begin
-                                dividend = -a;
-                                a_sign = 1;
-                        end
-
-                        if (b[OPERAND_WIDTH-1])
-                        begin
-                                divisor = -b;
-                                b_sign = 1;
-                        end     
-                end
-        end
-end
-
 // division algorithm iteration
 always_comb begin
         if (acc >= {1'b0, divisor})
@@ -88,39 +62,62 @@ always_ff @(posedge clk) begin
                 
                 quo <= 0;
                 acc <= 0;
+                divisor <= 0;
+                dividend <= 0;
                 i = 0;
         end
         else
         begin
                 done = 0;
-                
+
                 if (start) 
                 begin
                         valid = 0;
-                        if (divisor == 0) // catch divide by zero 
+                        if (a == 0) // catch divide by zero 
                         begin  
                                 busy <= 0;
                                 done <= 1;
                                 dbz <= 1;
                         end 
-                        else if (divisor > dividend)  // no need for multiple cycles
-                        begin
-                                busy <= 0;
-                                done <= 1;
-                                res <= 0;
-                                rem <= a;
-                                valid <= 1;
-                        end
+                        // else if (divisor > dividend)  // no need for multiple cycles
+                        // begin
+                        //         busy <= 0;
+                        //         done <= 1;
+                        //         res <= 0;
+                        //         rem <= a;
+                        //         valid <= 1;
+                        // end
                         else
                         begin
                                 busy <= 1;
                                 dbz <= 0;
                                 acc <= 0;
                                 quo <= '0;
-                                max_iter <= log2(dividend);
                                 i = 0;
+
+                                a_sign = 0;
+                                b_sign = 0;
+                                dividend = a;
+                                divisor = b;
+
+                                if (is_signed)
+                                begin
+                                        if (a[OPERAND_WIDTH-1])
+                                        begin
+                                                dividend = -a;
+                                                a_sign = 1;
+                                        end
+
+                                        if (b[OPERAND_WIDTH-1])
+                                        begin
+                                                divisor = -b;
+                                                b_sign = 1;
+                                        end
+                                end
+                                
+                                max_iter <= log2(dividend);
                         end
-                end 
+                end
                 else if (busy)
                 begin
                         if (i == max_iter + 2) // we're done, one extra cycle to set acc, quo
@@ -128,7 +125,6 @@ always_ff @(posedge clk) begin
                                 busy <= 0;
                                 done <= 1;
                                 valid <= 1;
-                                // Unshift the last
                                 res <= (a_sign ^ b_sign) ? -quo[OPERAND_WIDTH-1:1] : quo[OPERAND_WIDTH-1:1];
                                 rem <= (a_sign) ? -acc[OPERAND_WIDTH-1:1] : acc[OPERAND_WIDTH-1:1];                        end 
                         else 
