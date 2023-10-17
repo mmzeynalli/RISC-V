e0e0cc695801878f62df009f6e96b9abc22b9849
anshulrao0403 <51308061+anshulrao0403@users.noreply.github.com>
2023-10-17 17:54:06 +0200

    Added ILA module
    Created ILA module which compares register ad memory data

---
+++ design/RISC_V_ILA.sv
@@ -0,0 +1,39 @@
+module RISC_V_ILA (
+    input logic clk,                      // Clock signal
+    input logic rst,                      // Reset signal
+    input logic [31:0] riscv_registers [31:0],  // RISC-V register file
+    input logic [63:0] riscv_memory [63:0],     // RISC-V memory
+    input logic [31:0] expected_register_file [31:0],  // Expected register file data
+    input logic [63:0] expected_memory [63:0],     // Expected memory file data
+    output logic flag_registers_match,   // Flag indicating matching register files
+    output logic flag_memory_match       // Flag indicating matching memory contents
+);
+
+reg flag_registers_match_internal;
+reg flag_memory_match_internal;
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        flag_registers_match_internal <= 1'b0;
+        flag_memory_match_internal <= 1'b0;
+    end else begin
+        // Check for matching register files
+        if (riscv_registers === expected_register_file) begin
+            flag_registers_match_internal <= 1'b1;
+        end else begin
+            flag_registers_match_internal <= 1'b0;
+        end
+
+        // Check for matching memory contents
+        if (riscv_memory === expected_memory) begin
+            flag_memory_match_internal <= 1'b1;
+        end else begin
+            flag_memory_match_internal <= 1'b0;
+        end
+    end
+end
+
+assign flag_registers_match = flag_registers_match_internal;
+assign flag_memory_match = flag_memory_match_internal;
+
+endmodule
