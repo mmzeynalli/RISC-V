22f2da946b9ed9250fe44461763d3b3aeacc5a29
anshulrao0403 <51308061+anshulrao0403@users.noreply.github.com>
2023-10-18 00:09:42 +0200

    Update ila.sv
    Added the predefined register and memory values

--- design/ila.sv
+++ design/ila.sv
@@ -7,7 +7,23 @@ module ila_rm import common::*;
     output logic ok
 );
 
+
+///////////// TEST //////////////////////
+/*
+ADDI R1, R0, 20
+ADDI R2, R9, 3
+ADDI R0, R0, 0
+REM R3,R1,R2
+DIV R4,R1,R2
+SW R1, 0x1000(R0)   // Store the value of R1 at memory address 0x1000
+SW R2, 0x1004(R0)   // Store the value of R2 at memory address 0x1004
+SW R3, 0x1008(R0)   // Store the value of R3 at memory address 0x1008
+SW R4, 0x100C(R0)   // Store the value of R4 at memory address 0x100C
+*/
+//////////////////////////////////////////
+
 register_array expected_values;
+
 
 always_comb begin: rst_exp_regs
     if (rst == RESET)
@@ -18,6 +34,16 @@ always_comb begin: rst_exp_regs
         // ok = 0;
     end
 end
+
+
+// Store the expected register values after instruction execution
+    initial begin
+        // Initialize the expected_values array with 32-bit values for registers
+        expected_values[0] = 32'h00000014;  // R1
+        expected_values[1] = 32'h00000003;  // R2
+        expected_values[3] = 32'h00000002;  // R3
+        expected_values[4] = 32'h00000006;  // R4
+    end
 
 
 always_comb begin : check_regs
@@ -34,7 +60,9 @@ always_comb begin : check_regs
 end
 
 
+
 endmodule
+
 
 
 module ila_dm import common::*;
@@ -46,6 +74,8 @@ module ila_dm import common::*;
 
 memory_array expected_values;
 
+
+
 always_comb begin : rst_exp_mem
     if (rst == RESET)
     begin
@@ -55,6 +85,15 @@ always_comb begin : rst_exp_mem
         // ok = 0;
     end
 end
+
+// Store the expected memory values after instruction execution
+    initial begin
+        // Initialize the expected_values array with 32-bit values for memory
+        expected_values[0] = 32'h00000014;  // Memory value at address 0 (R1)
+        expected_values[1] = 32'h00000003;  // Memory value at address 4 (R2)
+        expected_values[2] = 32'h00000002;  // Memory value at address 8 (R3)
+        expected_values[3] = 32'h00000006;  // Memory value at address 12 (R4)
+    end
 
 
 always_comb begin : check_mem
@@ -67,5 +106,6 @@ always_comb begin : check_mem
             break;
         end
 end
+
 
 endmodule
